2df395823d0c8c800b9c73ddb4d5747a3ab25f10
Alexander Oborin <gaiwer@meta.ua>
2019-08-08 16:04:36 +0300

    Add test.php

---
+++ person.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alex
+ * Date: 08.08.19
+ * Time: 11:50
+ */
+
+var_dump("Person_1");
+
+?>
---
+++ test.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alex
+ * Date: 08.08.19
+ * Time: 15:57
+ */
+
+echo("Test!");
+
+?>
